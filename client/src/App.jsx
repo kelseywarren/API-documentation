690a98400a71ce68f00b69b83704f66875e73bd8
kelseywarren <warrenkelsey@outlook.com>
2024-06-16 22:24:51 -0500

    Test route working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import './App.css'
+import Login from './pages/login/login';
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/login" element={ <Login/> }>
+
+  </Route>
+));
 function App() {
 
-  const router = createBrowserRouter();
-  
   return (
     <>
-      <h1>Medical Laboratory Science</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,7 +1,11 @@
 import React from 'react'; 
 
 function Login() {
-
+    return (
+        <>
+        <p>Test Login</p>
+        </>
+    )
 };
 
 export default Login;
