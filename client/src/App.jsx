33b2c0325a97e383fb1f305a51556c046463fd19
kelseywarren <warrenkelsey@outlook.com>
2024-06-16 21:51:07 -0500

    Folder and router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
-
+  const router = createBrowserRouter();
+  
   return (
     <>
       <h1>Medical Laboratory Science</h1>
