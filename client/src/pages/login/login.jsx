dc4ad4aefa6b77a881f98c4aa14c87c15ef5c62f
kelseywarren <warrenkelsey@outlook.com>
2024-06-18 18:57:21 -0500

    Add basic login form for test purposes

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Login() {
     return (
-        <>
-        <p>Login</p>
-        </>
+        <form>
+            <input className="enterEmail" type="text" placeholder="email"></input><br></br>
+            <input className="enterPassword" type="text" placeholder="password"></input><br></br>
+            <button className="loginButton" type="submit">log in</button>
+        </form>
     )
 };
 
