7ce92dd902dad5de4cb8b3f0ab65740d8e9a84c1
kelseywarren <warrenkelsey@outlook.com>
2024-06-19 21:26:43 -0500

    Base setup for register

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Register () {
+    const [username, setUsername] = useState(''); //store username
+    const [email, setEmail] = useState(''); // store email
+    const [password, setPassword] = useState(''); // store password
+
     return (
-        <p>register</p>
+       <div>
+            <form>
+                <input
+                placeholder="username"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <br></br>
+                <input
+                placeholder="email"
+                type="text"
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <br></br>
+                <input
+                placeholder="password"
+                type="text"
+                onChange={(e) => setPassword(e.target.value)}
+                />
+            </form>
+       </div>
+
     )
 };
 
