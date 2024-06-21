20c443eb608d1d79fae34580fde8f58914c0351e
kelseywarren <warrenkelsey@outlook.com>
2024-06-20 21:40:24 -0500

    Add more base details to register form

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -7,27 +7,43 @@ function Register () {
     const [password, setPassword] = useState(''); // store password
 
     return (
-       <div>
-            <form>
-                <input
-                placeholder="username"
-                type="text"
-                onChange={(e) => setUsername(e.target.value)}
-                />
-                <br></br>
-                <input
-                placeholder="email"
-                type="text"
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <br></br>
-                <input
-                placeholder="password"
-                type="text"
-                onChange={(e) => setPassword(e.target.value)}
-                />
-            </form>
-       </div>
+        <div>
+        <form>
+          <label for="username">Username</label>
+          <br></br>
+            <input
+            placeholder="username..."
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            className="username"
+            required
+            /> 
+            <br></br>
+          <label for="email">Email</label>
+          <br></br>
+            <input 
+            placeholder="email..."
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            className="email"
+            required
+            />
+            <br></br>
+          <label for="password">Password</label>
+          <br></br>
+            <input
+            placeholder="password..."
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+            className="password"
+            required
+            />
+          <br></br><br></br>
+          <button onClick={() => {
+            validateForm()
+          }}>Create Account</button>
+        </form>
+   </div>
 
     )
 };
