3292d6c2e3e375239dfad7f2ecb93867674df4da
kelseywarren <warrenkelsey@outlook.com>
2024-06-22 11:55:18 -0500

    Add base controller for registering user

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,7 +1,44 @@
-const test = (req, res) => {
+const User = require('../models/users');
+
+function test(req, res) {
     res.json('test route working')
 };
 
+async function registerUser (req, res) {
+  try {
+    const {username, email, password} = req.body;
+    if (!username) {
+        return res.json({error: 'username is required'});
+    };
+    if (!email) {
+        return res.json({error: 'email address is required'})
+    };
+    if (!password) {
+        return res.json({error: 'password is required'})
+    };
+    if (password.length < 8) {
+        return res.json({error: 'password must be at least 8 characters long'})
+    };
+
+    const exist = await User.findOne({email});
+    if (exist) {
+        return res.json({error: 'email address already exists'})
+    };
+
+    const user = await User.create({
+        username, email, password
+    });
+
+    return res.json(user);
+
+
+  } catch (error) {
+    console.log(error)
+  }  
+};
+
+
 module.exports = {
-    test
+    test,
+    registerUser
 };
