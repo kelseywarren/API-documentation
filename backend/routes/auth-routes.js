b04246c57584adc407c884a513fa3f56d069320b
kelseywarren <warrenkelsey@outlook.com>
2024-06-22 22:38:41 -0500

    Add comments for auth-routes file

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,14 +1,20 @@
+// Import express, router, and cors 
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
+
+// Import controllers 
 const { test, registerUser } = require('../controllers/auth-controller');
 
+// Handle cors
 router.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
 }));
 
+// Routes
 router.get('/', test)
 router.post('/register', registerUser)
 
+// Export router
 module.exports = router; 
