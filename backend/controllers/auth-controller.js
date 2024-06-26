f3f607630795eca7b674397ba3074a6e506e176d
kelseywarren <warrenkelsey@outlook.com>
2024-06-25 19:24:12 -0500

    Refactor auth controller

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -10,95 +10,94 @@ function test(req, res) {
 async function registerUser (req, res) {
   try {
     const {username, email, password} = req.body;
+    // Password requirement variables
+    const lowerCaseCheck = /(?=.*[a-z])/;
+    const upperCaseCheck = /(?=.*[A-Z])/;
+    const numberCheck = /(?=.*[0-9])/;
+    const specialCheck = /(?=.*[!@#$%^&*])/;
+    const passwordLengthCheck = /(?=.{8,})/;
+    
+    //Check if form is empty
+    if (!username && !email && !password) {
+        return res.json({
+            error: 'username, email, and password are required'
+        })
+    }
+
+    // Check if username field is empty
     if (!username) {
         return res.json({
             error: 'username is required'
         })
     };
+    // Check if email field is empty
     if (!email) {
         return res.json({
             error: 'email address is required'
         })
     };
+    // Check if password field is empty
     if (!password) {
         return res.json({
-            error: 'password is required'
-        })
-    };
-    if (password.length < 8) {
-        return res.json({
-            error: 'password must be at least 8 characters long'
+            error: 'password is required and must contain at least one lowercase, uppercase, numerical, and special character'
         })
     };
 
-    //variables
-    let countUpperCase = 0;
-    let countLowerCase = 0;
-    let countDigit = 0;
-    let countSpecialChar = 0;
-
-
-    // Loop to check each password
-    for (let i = 0; i < password.length; i++) {
-
-    const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', '{', ']', '}', ':', ';', '<', '>']
-    // Check if password contains uppercase letter
-    if (password[i] == password[i].toUpperCase()) {
-        countUpperCase++; 
-    };
-
-    // Check if password contains lowercase letter
-    if (password[i] == password[i].toLowerCase()) {
-        countLowerCase++;
-    };
-
-    // Check if password contains a number
-    if (!isNaN(password[i] * 1)) {
-        countDigit++;
-    };
-    
-    // Check if password contains a special character
-    if (specialChars.includes(password[i])) {
-    countSpecialChar++;
-    };
-
-    }; 
-
-    // Check if password is missing uppercase letter 
-    if (countUpperCase == 0) {
-    res.json({error: 'password must contain at least one upper case character'})
-    };
-    // Check if password is missing lowercase letter
-    if (countLowerCase == 0) {
-    res.json({error: 'password must contain at least one lower case character'})
-    };
-
-    // Check if password is missing number
-    if (countDigit == 0) {
-    res.json({error: 'password must contain at least one numerical digit'})
-    };
-
-    // Check if password is missing special character
-    if (countSpecialChar == 0) {
-    res.json({error: 'password must contain at least one special character'}) 
-    };
+    // Check if password has upper case letter
+    if (!password.match(lowerCaseCheck)) {
+    return res.json({
+        error: "password must contain a lower case letter",
+    });
+    }
+    // Check if password has lower case letter
+    if (!password.match(upperCaseCheck)) {
+    return res.json({
+        error: "password must contain an uppercase case letter",
+    });
+    }
+    // Check if password has number
+    if (!password.match(numberCheck)) {
+    return res.json({
+        error: "password must contain a number",
+    });
+    }
+    // Check if password has special character
+    if (!password.match(specialCheck)) {
+    return res.json({
+        error:
+        "password must contain one of the following special characters: !@#$%^&*",
+    });
+    }
+    // Check if password is 8 characters long
+    if (!password.match(passwordLengthCheck)) {
+    return res.json({
+        error: "password must be at least 8 characters long",
+    });
+    }
 
     // Check if user name already exist
     const usernameExist = await User.findOne({username});
     if (usernameExist) {
-        return res.json({error: 'username already exists'})
+        return res.json({
+            error: 'username already exists'
+        })
     };   
 
     // Check if email address already exist
     const emailExist = await User.findOne({email});
     if (emailExist) {
-        return res.json({error: 'email address already exists'})
+        return res.json({
+            error: 'email address already exists'
+        })
     };
 
     // Create user once all conditions satisfied
     const user = await User.create({
-        username, email, password
+        username, 
+        email, 
+        password,
     });
+
 
     // Send user details as response 
     return res.json(user);
