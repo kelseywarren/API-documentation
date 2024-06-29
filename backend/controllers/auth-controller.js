65e33760fdb971249be2e97203b8f9a8df94a9b3
kelseywarren <warrenkelsey@outlook.com>
2024-06-28 22:03:51 -0500

    Add getLoggedInUser controller

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -185,10 +185,27 @@ async function loginUser(req, res) {
       console.log(error);
     }
   };
+
+  // Get username from session stored on mongodb 
+  async function getLoggedInUser(req, res)  {
+    try {
+      if (req.session.user) {
+        // Retrieve user details from session
+        const { username } = req.session.user;
+        res.json({ username });
+      } else {
+        res.status(401).json({ error: 'user not logged in' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'server error' });
+    }
+  };
   
 // Export controller functions to be used in routes
 module.exports = {
     test,
     registerUser,
-    loginUser
+    loginUser,
+    getLoggedInUser
 };
